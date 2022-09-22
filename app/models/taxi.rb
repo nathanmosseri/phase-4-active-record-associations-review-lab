aef2048bd23d80b02668cb8c7de22c84b7a158f1
Nathan <namo678@me.com>
2022-09-22 10:51:39 -0400

    commit tree 14e4eed1831d94cbf8fae14e5fc3fb227182110a

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,7 @@
 class Taxi < ApplicationRecord
+
+    has_many :rides
+    has_many :passengers, through: :rides 
+    
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Taxi.create(name: 'yellow taxi')
+Taxi.create(name: 'blue taxi')
+Taxi.create(name: 'green taxi')
+
+Passenger.create(name: 'nathan')
+Passenger.create(name: 'david')
+Passenger.create(name: 'guy')
+
+6.times do 
+    Ride.create(taxi_id: Taxi.all.sample.id, passenger_id: Passenger.all.sample.id)
+end
